03c06fab4229bda99881ebcb7ad491a9ec89c584
synx <synx@trustless.computer>
2024-01-18 13:49:46 +0700

    .

--- ui/snippets/footer/Footer.tsx
+++ ui/snippets/footer/Footer.tsx
@@ -1,3 +1,8 @@
+/* eslint-disable space-before-function-paren */
+/* eslint-disable react/jsx-tag-spacing */
+/* eslint-disable template-curly-spacing */
+/* eslint-disable array-bracket-spacing */
+/* eslint-disable react/jsx-curly-spacing */
 import type { GridProps } from '@chakra-ui/react';
 import { Box, Grid, Flex, Text, Link, VStack, Skeleton } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
@@ -18,8 +23,8 @@ import getApiVersionUrl from './utils/getApiVersionUrl';
 
 const MAX_LINKS_COLUMNS = 4;
 
-const FRONT_VERSION_URL = `https://github.com/blockscout/frontend/tree/${ config.UI.footer.frontendVersion }`;
-const FRONT_COMMIT_URL = `https://github.com/blockscout/frontend/commit/${ config.UI.footer.frontendCommit }`;
+const FRONT_VERSION_URL = `https://github.com/blockscout/frontend/tree/${config.UI.footer.frontendVersion}`;
+const FRONT_COMMIT_URL = `https://github.com/blockscout/frontend/commit/${config.UI.footer.frontendCommit}`;
 
 const Footer = () => {
 
@@ -77,11 +82,11 @@ const Footer = () => {
 
   const frontendLink = (() => {
     if (config.UI.footer.frontendVersion) {
-      return <Link href={ FRONT_VERSION_URL } target="_blank">{ config.UI.footer.frontendVersion }</Link>;
+      return <Link href={FRONT_VERSION_URL} target="_blank">{config.UI.footer.frontendVersion}</Link>;
     }
 
     if (config.UI.footer.frontendCommit) {
-      return <Link href={ FRONT_COMMIT_URL } target="_blank">{ config.UI.footer.frontendCommit }</Link>;
+      return <Link href={FRONT_COMMIT_URL} target="_blank">{config.UI.footer.frontendCommit}</Link>;
     }
 
     return null;
@@ -90,8 +95,8 @@ const Footer = () => {
   const fetch = useFetch();
 
   const { isPlaceholderData, data: linksData } = useQuery<unknown, ResourceError<unknown>, Array<CustomLinksGroup>>({
-    queryKey: [ 'footer-links' ],
-    queryFn: async() => fetch(config.UI.footer.links || '', undefined, { resource: 'footer-links' }),
+    queryKey: ['footer-links'],
+    queryFn: async () => fetch(config.UI.footer.links || '', undefined, { resource: 'footer-links' }),
     enabled: Boolean(config.UI.footer.links),
     staleTime: Infinity,
     placeholderData: [],
@@ -102,41 +107,41 @@ const Footer = () => {
   const renderNetworkInfo = React.useCallback((gridArea?: GridProps['gridArea']) => {
     return (
       <Flex
-        gridArea={ gridArea }
+        gridArea={gridArea}
         flexWrap="wrap"
-        columnGap={ 8 }
-        rowGap={ 6 }
+        columnGap={8}
+        rowGap={6}
         mb={{ base: 5, lg: 10 }}
         _empty={{ display: 'none' }}
       >
-        { !config.UI.indexingAlert.intTxs.isHidden && <IntTxsIndexingStatus/> }
-        <NetworkAddToWallet/>
+        {!config.UI.indexingAlert.intTxs.isHidden && <IntTxsIndexingStatus />}
+        <NetworkAddToWallet />
       </Flex>
     );
   }, []);
 
   const renderProjectInfo = React.useCallback((gridArea?: GridProps['gridArea']) => {
     return (
-      <Box gridArea={ gridArea }>
+      <Box gridArea={gridArea}>
         <Link fontSize="xs" href="https://bitcoinarcade.xyz">bitcoinarcade.xyz</Link>
-        <Text mt={ 3 } fontSize="xs">
+        <Text mt={3} fontSize="xs">
           Ushering the golden era of Gaming on Bitcoin
         </Text>
-        <VStack spacing={ 1 } mt={ 6 } alignItems="start" hidden>
-          { apiVersionUrl && (
+        <VStack spacing={1} mt={6} alignItems="start" hidden>
+          {apiVersionUrl && (
             <Text fontSize="xs">
-              Backend: <Link href={ apiVersionUrl } target="_blank">{ backendVersionData?.backend_version }</Link>
+              Backend: <Link href={apiVersionUrl} target="_blank">{backendVersionData?.backend_version}</Link>
             </Text>
-          ) }
-          { frontendLink && (
+          )}
+          {frontendLink && (
             <Text fontSize="xs">
-              Frontend: { frontendLink }
+              Frontend: {frontendLink}
             </Text>
-          ) }
+          )}
         </VStack>
       </Box>
     );
-  }, [ apiVersionUrl, backendVersionData?.backend_version, frontendLink ]);
+  }, [apiVersionUrl, backendVersionData?.backend_version, frontendLink]);
 
   const containerProps: GridProps = {
     as: 'footer',
@@ -150,18 +155,18 @@ const Footer = () => {
 
   if (config.UI.footer.links) {
     return (
-      <Grid { ...containerProps }>
+      <Grid {...containerProps}>
         <div>
-          { renderNetworkInfo() }
-          { renderProjectInfo() }
+          {renderNetworkInfo()}
+          {renderProjectInfo()}
         </div>
 
         <Grid
           gap={{ base: 6, lg: colNum === MAX_LINKS_COLUMNS + 1 ? 2 : 8, xl: 12 }}
           gridTemplateColumns={{
             base: 'repeat(auto-fill, 160px)',
-            lg: `repeat(${ colNum }, 135px)`,
-            xl: `repeat(${ colNum }, 160px)`,
+            lg: `repeat(${colNum}, 135px)`,
+            xl: `repeat(${colNum}, 160px)`,
           }}
           justifyContent={{ lg: 'flex-end' }}
           mt={{ base: 8, lg: 0 }}
@@ -173,10 +178,10 @@ const Footer = () => {
             ])
               .slice(0, colNum)
               .map(linkGroup => (
-                <Box key={ linkGroup.title }>
-                  <Skeleton fontWeight={ 500 } mb={ 3 } display="inline-block" isLoaded={ !isPlaceholderData }>{ linkGroup.title }</Skeleton>
-                  <VStack spacing={ 1 } alignItems="start">
-                    { linkGroup.links.map(link => <FooterLinkItem { ...link } key={ link.text } isLoading={ isPlaceholderData }/>) }
+                <Box key={linkGroup.title}>
+                  <Skeleton fontWeight={500} mb={3} display="inline-block" isLoaded={!isPlaceholderData}>{linkGroup.title}</Skeleton>
+                  <VStack spacing={1} alignItems="start">
+                    {linkGroup.links.map(link => <FooterLinkItem {...link} key={link.text} isLoading={isPlaceholderData} />)}
                   </VStack>
                 </Box>
               ))
@@ -188,7 +193,7 @@ const Footer = () => {
 
   return (
     <Grid
-      { ...containerProps }
+      {...containerProps}
       gridTemplateAreas={{
         lg: `
           "network links-top"
@@ -197,12 +202,12 @@ const Footer = () => {
       }}
     >
 
-      { renderNetworkInfo({ lg: 'network' }) }
-      { renderProjectInfo({ lg: 'info' }) }
+      {renderNetworkInfo({ lg: 'network' })}
+      {renderProjectInfo({ lg: 'info' })}
 
       <Grid
         gridArea={{ lg: 'links-bottom' }}
-        gap={ 1 }
+        gap={1}
         gridTemplateColumns={{
           base: 'repeat(auto-fill, 160px)',
           lg: 'repeat(3, 160px)',
@@ -218,7 +223,7 @@ const Footer = () => {
         justifyContent={{ lg: 'flex-end' }}
         mt={{ base: 8, lg: 0 }}
       >
-        { BLOCKSCOUT_LINKS.map(link => <FooterLinkItem { ...link } key={ link.text }/>) }
+        {BLOCKSCOUT_LINKS.map(link => <FooterLinkItem {...link} key={link.text} />)}
       </Grid>
     </Grid>
   );
